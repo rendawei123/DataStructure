0819269d57184a55e7d99ed527c808f4cf362816
rendawei123 <lovedavidren@outlook.com>
2017-11-01 20:24:19 +0800

    add graph about adjoining

--- graph.py
+++ graph.py
@@ -62,14 +62,92 @@ class Graph:
     def __str__(self):
         return "[\n" + ",\n".join(map(str,self._mat)) + "\n]" + "\nUnconnected:" + str(self._unconn)
 
-if __name__ == '__main__':
-    a = [
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0]
-    ]
+"""
+压缩的邻接矩阵（邻接表）实现
 
-    g = Graph(a)
-    print(str(g))
+邻接矩阵的缺点是空间占用与顶点数的平方成正比，可能带来很大的浪费，
+另外，邻接矩阵不容易实现增加顶点，不太适合以逐步扩充的方式构造图对象
+
+邻接表实现的优点
+
+每个顶点v的所有临接边用一个（不一定等长）的list对象表示，元素形式为（v',w）,其中v'是边的终点，w是边的信息。一个图对象的主要部分就是这种list的list，每个元素对应一个顶点
+
+这种设计很容易给已有的图添加顶点。为此只需要在外层表中增加一个表示新顶点的项，对应的边表设置为空表，通过随后的操作加入边
+
+这种表示能更好地支持以逐步扩充的方式构造大型图对象
+"""
+
+class GraphAdjoining(Graph):
+    def __init__(self, mat=[], unconn=0):
+        vnum = len(mat)  # 顶点数
+        for x in mat:  # 判断是否为方阵
+            if len(x) != vnum:
+                raise ValueError("Argument for 'Graph'")
+
+        # 由于参数是列表复制一份
+        self._mat = [Graph._out_edges(mat[i], unconn) for i in range(vnum)]
+        self._vnum = vnum  # 顶点数
+        self._unconn = unconn  # 边数
+
+    # 增加新结点
+    def add_vertex(self):
+        self._mat.append([])
+        self._vnum += 1
+        return self._vnum - 1  # 返回一个新编号
+
+    # 添加一个边
+    def add_edge(self, vi, vj, val=1):
+        if self._vnum == 0: # 如果顶点数为零
+            raise ValueError('Can not add edge to empty Graph')
+
+        if self._invalid(vi) or self._invalid(vj):  # 如果顶点无意义
+            raise ValueError(str(vi) + 'or' + str(vj) + 'is not valid vertex')
+
+        row = self._mat[vi]
+        i = 0
+        while i < len(row):
+            if row[i][0] == vj:  # 如果有边，重新赋值
+                self._mat[vi][i] = (vi, val)
+                return
+            if row[i][0] > vj:
+                break
+            i += 1
+
+        self._mat[vi].insert(i, (vj, val))
+
+    # 获取边
+    def get_edge(self, vi, vj):
+        if self._invalid(vi) or self._invalid(vj):  # 如果顶点无意义
+            raise ValueError(str(vi) + 'or' + str(vj) + 'is not valid vertex')
+
+        for i, val in self._mat[vi]:
+            if i == vi:
+                return val
+
+        return self._unconn
+
+    # 获取出边
+    def out_edges(self, vi):
+        if self._invalid(vi):  # 如果顶点无意义
+            raise ValueError(str(vi) + 'is not valid vertex')
+
+        return self._mat[vi]
+
+
+if __name__ == '__main__':
+    # a = [
+    # [0,0,0,0,0],
+    # [0,0,0,0,0],
+    # [0,0,0,0,0],
+    # [0,0,0,0,0],
+    # [0,0,0,0,0]
+    # ]
+
+    # g = Graph(a)
+    # print(str(g))
+    b = GraphAdjoining()
+    b.add_vertex()
+    b.add_vertex()
+    b.add_edge(0,1)
+    b.add_edge(1,0)
+    print(str(b))
